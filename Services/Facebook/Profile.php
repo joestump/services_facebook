729185b78543d26470132b89fab07272afa99f36
jeffhodsdon <jeffhodsdon@e4a961c0-893e-0410-870e-adc8fef1e38a>
2008-04-03 05:22:09 +0000

    Update to profile->setFBML parameters.  Added option to specify session key.

--- Services/Facebook/Profile.php
+++ Services/Facebook/Profile.php
@@ -33,18 +33,43 @@ class Services_Facebook_Profile extends Services_Facebook_Common
     /**
      * Set FBML in a user's profile
      *
-     * @param       string      $markup     FBML markup language to set
-     * @param       int         $uid        Facebook uid to set FBML for
+     * You are not required to use a session key that belongs
+     * to the user whose profile you are changing.
+     *
+     * @param       string      $profileMarkup      FBML for the profile box
+     * @param       string      $profileAction      FBML for the profile actions
+     * @param       string      $mobileProfile      FBML for Facebook mobile view
+     * @param       int         $uid                Facebook uid to set FBML for
+     * @param       string      $sessionKey         The sessionKey that may be 
+     *                                               specified
      * @return      boolean     True on success, false on unknown error
      * @link        http://wiki.developers.facebook.com/index.php/Profile.setFBML
      */
-    public function setFBML($markup, $uid = 0)
+    public function setFBML($profileMarkup = '', 
+                            $profileAction = '', 
+                            $mobileProfile = '', 
+                            $uid = 0,
+                            $sessionKey = '')
     {
-        $args = array('markup' => $markup,
-                      'session_key' => $this->sessionKey);
+        $args = array();
 
+        if (strlen($profileMarkup)) {
+            $args['profile'] = $profileMarkup;
+        }
+        if (strlen($profileAction)) {
+            $args['profile_action'] = $profileAction;
+        }
+        if (strlen($mobileProfile)) {
+            $args['mobile_profile'] = $mobileProfile;
+        }
         if ($uid > 0) {
             $args['uid'] = $uid;
+        }
+        if (strlen($sessionKey)) {
+            $args['session_key'] = $sessionKey;
+        }
+        else {
+            $args['session_key'] = $this->sessionKey;
         }
 
         $result = $this->sendRequest('profile.setFBML', $args);
