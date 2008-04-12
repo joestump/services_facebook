eb8c6a1a65be2f3b802458f57a4690b274d95456
jstump <jstump@e4a961c0-893e-0410-870e-adc8fef1e38a>
2008-04-12 21:49:19 +0000

    Merged out r45, which did a lot of breaking of stuff.

--- Services/Facebook/Common.php
+++ Services/Facebook/Common.php
@@ -11,7 +11,6 @@
  * a copy of the New BSD License and are unable to obtain it through the web, 
  * please send a note to license@php.net so we can mail you a copy immediately.
  *
-<<<<<<< HEAD:Services/Facebook/Common.php
  * @category  Services
  * @package   Services_Facebook
  * @author    Joe Stump <joe@joestump.net> 
@@ -19,32 +18,16 @@
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @version   CVS: $Id$
  * @link      http://pear.php.net/package/Services_Facebook
-=======
- * @category    Services
- * @package     Services_Facebook
- * @author      Joe Stump <joe@joestump.net> 
- * @copyright   Joe Stump <joe@joestump.net>  
- * @license     http://www.opensource.org/licenses/bsd-license.php 
- * @version     CVS: $Id:$
- * @link        http://pear.php.net/package/Services_Facebook
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
  */
 
 /**
  * Common class for all Facebook interfaces
  *
-<<<<<<< HEAD:Services/Facebook/Common.php
  * @category Services
  * @package  Services_Facebook
  * @author   Joe Stump <joe@joestump.net>
  * @license  http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @link     http://wiki.developers.facebook.com
-=======
- * @category    Services
- * @package     Services_Facebook
- * @author      Joe Stump <joe@joestump.net>
- * @link        http://wiki.developers.facebook.com
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
  */
 abstract class Services_Facebook_Common
 {
@@ -83,37 +66,20 @@ abstract class Services_Facebook_Common
      * 
      * Any formal error encountered is thrown as an exception.
      *
-<<<<<<< HEAD:Services/Facebook/Common.php
      * @param string $method The API method to call
      * @param array  $args   API arguments passed as GET args
      *
      * @return object Response as an instance of SimleXmlElement
      * @throws Services_Facebook_Exception
-=======
-     * @access      protected
-     * @param       string      $method     The API method to call
-     * @param       array       $args       API arguments passed as GET args
-     * @return      object      Response as an instance of SimleXmlElement
-     * @throws      Services_Facebook_Exception
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
      */
     protected function sendRequest($method, array $args = array()) 
     {
         $args['api_key'] = Services_Facebook::$apiKey;
-<<<<<<< HEAD:Services/Facebook/Common.php
         $args['v']       = $this->version;
         $args['format']  = 'XML';
         $args['method']  = $method;
         $args['call_id'] = microtime(true);
         $args            = $this->signRequest($args);
-=======
-        $args['v'] = $this->version;
-        $args['format'] = 'XML';
-        $args['method'] = $method;
-        $args['call_id'] = microtime(true);
-        $args['session_key'] = $this->sessionKey;
-        $args = $this->signRequest($args);
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->api);
@@ -124,26 +90,18 @@ abstract class Services_Facebook_Common
         $result = curl_exec($ch);
 
         if (curl_errno($ch)) {
-<<<<<<< HEAD:Services/Facebook/Common.php
             throw new Services_Facebook_Exception(
                 curl_error($ch), curl_errno($ch), $url, $this->api
             );
-=======
-            throw new Services_Facebook_Exception(curl_error($ch), curl_errno($ch), $url, $this->api);
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
         }
 
         curl_close($ch);
 
         $xml = @simplexml_load_string($result);
         if (!$xml instanceof SimpleXmlElement) {
-<<<<<<< HEAD:Services/Facebook/Common.php
             throw new Services_Facebook_Exception(
                 'Could not parse XML response', 0, $this->api
             );
-=======
-            throw new Services_Facebook_Exception('Could not parse XML response', 0, $this->api);
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
         }
 
         $error = $this->checkRequest($xml);
@@ -158,16 +116,10 @@ abstract class Services_Facebook_Common
     /**
      * Sign the request
      *
-<<<<<<< HEAD:Services/Facebook/Common.php
      * @param array $args Arguments for the request to be signed
      * 
      * @return array Arguments with the appropriate sig added
      * @see Services_Facebook::$secret
-=======
-     * @param       array       $args
-     * @return      array       Arguments with the appropriate sig added
-     * @see         Services_Facebook::$secret
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
      */
     protected function signRequest(array $args) 
     {
@@ -182,11 +134,7 @@ abstract class Services_Facebook_Common
             $sig .= $k .'=' . $v;
         }
 
-<<<<<<< HEAD:Services/Facebook/Common.php
         $sig        .= Services_Facebook::$secret;
-=======
-        $sig .= Services_Facebook::$secret;
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
         $args['sig'] = md5($sig);
         return $args;
     }
@@ -194,7 +142,6 @@ abstract class Services_Facebook_Common
     /**
      * Check if request resulted in an error
      *
-<<<<<<< HEAD:Services/Facebook/Common.php
      * @param object $xml Instance of SimpleXmlElement
      * 
      * @return Array with code/message or false if no error is present
@@ -203,15 +150,6 @@ abstract class Services_Facebook_Common
     {
         $message = null;
         $code    = 0;
-=======
-     * @param       object      $xml        Instance of SimpleXmlElement
-     * @return      Array with code/message or false if no error is present
-     */
-    private function checkRequest($xml)
-    {
-        $message = null;
-        $code = 0;
->>>>>>> 209a1c089f04360c2632bb0dc6a8cad33cc6a8bb:Services/Facebook/Common.php
         switch ($this->version) {
         case '1.0':
             if (isset($xml->error_code)) {
